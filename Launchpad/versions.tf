1bc7215ba5ec95a3b798cb9f8f56d2aad0bd4b68
bencreasy <ben@ghostlab.net>
2025-02-09 00:35:56 -0500

    Create versions.tf

---
+++ Launchpad/versions.tf
@@ -0,0 +1,16 @@
+### versions.tf
+```hcl
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.0"
+    }
+  }
+}
+```
