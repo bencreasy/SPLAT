3d634943c4181afedad8d1b7842096ad7c7e74a1
bencreasy <ben@ghostlab.net>
2025-02-09 00:37:48 -0500

    Create providers.tf

---
+++ Launchpad/providers.tf
@@ -0,0 +1,14 @@
+### providers.tf
+```hcl
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+provider "google-beta" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+```
