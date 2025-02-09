398bd121b8429759e4e6f1073c09be9823ccc49d
bencreasy <ben@ghostlab.net>
2025-02-09 02:42:41 -0500

    Create variables.tf

---
+++ Launchpad/Modules/Ground_station/variables.tf
@@ -0,0 +1,37 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment (dev, staging, prod)"
+  type        = string
+}
+
+variable "region" {
+  description = "The region for resources"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "function_memory" {
+  description = "Memory allocation for Cloud Functions (MB)"
+  type        = number
+  default     = 256
+}
+
+variable "telemetry_topic_id" {
+  description = "The ID of the telemetry Pub/Sub topic"
+  type        = string
+}
+
+variable "device_state_topic_id" {
+  description = "The ID of the device state Pub/Sub topic"
+  type        = string
+}
+
+variable "data_retention_days" {
+  description = "Number of days to retain processed data"
+  type        = number
+  default     = 30
+}
