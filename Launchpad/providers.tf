6b6cde6ce41208b739194f8408b2f2f06f04671a
bencreasy <ben@ghostlab.net>
2025-02-10 02:26:31 -0500

    Update providers.tf

--- Launchpad/providers.tf
+++ Launchpad/providers.tf
@@ -1,12 +1,12 @@
 
 provider "google" {
-  project = kinetic-abbey-450502-d6
+  project = "kinetic-abbey-450502-d6"
   region  = var.region
   zone    = var.zone
 }
 
 provider "google-beta" {
-  project = kinetic-abbey-450502-d6
+  project = "kinetic-abbey-450502-d6"
   region  = var.region
   zone    = var.zone
 }
