32daebbac75ae6264c467c9fde2bba60ce258ff6
bencreasy <ben@ghostlab.net>
2025-02-10 02:31:38 -0500

    Update providers.tf

--- Launchpad/providers.tf
+++ Launchpad/providers.tf
@@ -3,6 +3,7 @@ provider "google" {
   project = "kinetic-abbey-450502-d6"
   region  = var.region
   zone    = var.zone
+  credentials = file("C:\Users\ben\Downloads\kinetic-abbey-450502-d6-b32c7c308785.json")
 }
 
 provider "google-beta" {
