0ad9c028db19380864c29fdf1584cab1ef680f76
bencreasy <ben@ghostlab.net>
2025-02-10 23:50:06 -0500

    Update cred

--- Launchpad/providers.tf
+++ Launchpad/providers.tf
@@ -3,6 +3,7 @@ provider "google" {
   project = "kinetic-abbey-450502-d6"
   region  = var.region
   zone    = var.zone
+  credentials = "${path.module}/spal-key.json"
 }
 
 provider "google-beta" {
