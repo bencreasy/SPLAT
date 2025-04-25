a946a6ae2be782bfdb66c6a59d3383b6e87df386
bencreasy <ben@ghostlab.net>
2025-04-25 18:10:02 -0400

    Create __init__.py

---
+++ FlightDirector/EdenControl/Communication/__init__.py
@@ -0,0 +1,8 @@
+"""
+Communication components for Eden Control System.
+Provides cloud connectivity and data synchronization.
+"""
+
+from .cloud_manager import CloudManager
+
+__all__ = ['CloudManager']
